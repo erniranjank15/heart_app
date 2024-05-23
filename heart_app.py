de89b2005585d753f0fde5d9429346eb5004e310
erniranjank15 <kasoteniranjan23@gmail.com>
2024-05-23 09:41:39 +0530

    heart_app.py

--- heart_app.py
+++ heart_app.py
@@ -1,11 +1,11 @@
 import pickle
 import streamlit as st
 
-# loading the saved models
-heart_disease_model = pickle.load(open('trained_model.sav', 'rb'))
+# Load the saved model
+heart_disease_model = pickle.load(open('C:/Users/kasot/Desktop/mini project/trained_model.sav', 'rb'))
 
-# Check if the 'Heart Disease Prediction' page is selected
-selected = 'Heart Disease Prediction'  # This is just an example. You'll need to replace this with actual logic to handle multiple pages.
+# Assuming 'selected' is defined somewhere in your code
+selected = 'Heart Disease Prediction'  # Set this variable as needed
 
 # Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
@@ -13,46 +13,35 @@ if selected == 'Heart Disease Prediction':
     # Page title
     st.title('Heart Disease Prediction using ML')
 
+    # Input fields
     col1, col2, col3 = st.columns(3)
 
     with col1:
         age = st.text_input('Age')
-
     with col2:
         sex = st.text_input('Sex')
-
     with col3:
         cp = st.text_input('Chest Pain types')
-
     with col1:
         trestbps = st.text_input('Resting Blood Pressure')
-
     with col2:
         chol = st.text_input('Serum Cholestoral in mg/dl')
-
     with col3:
         fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-
     with col1:
         restecg = st.text_input('Resting Electrocardiographic results')
-
     with col2:
         thalach = st.text_input('Maximum Heart Rate achieved')
-
     with col3:
         exang = st.text_input('Exercise Induced Angina')
-
     with col1:
         oldpeak = st.text_input('ST depression induced by exercise')
-
     with col2:
         slope = st.text_input('Slope of the peak exercise ST segment')
-
     with col3:
         ca = st.text_input('Major vessels colored by flourosopy')
-
     with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversible defect')
 
     # Code for Prediction
     heart_diagnosis = ''
